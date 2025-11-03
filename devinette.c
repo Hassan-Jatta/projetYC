3a4c6258f7efc83f58990a2c3d2788406b6cffe5
Hassan Jatta <188450546+Hassan-Jatta@users.noreply.github.com>
2025-11-03 17:19:25 +0100

    celebration succès

--- devinette.c
+++ devinette.c
@@ -3,11 +3,16 @@
 int main() {
 	int nb_secret = 32;
 	int nb_user;
+    printf("Devinez le nombre : ");
 	scanf("%d", &nb_user);
 	if (nb_user == nb_secret) {
-		printf("Succès");
-	} else {
+        printf("\033[1;34m"); 
+        printf("Bravo !!! Vous avez trouvé le bon nombre (%d) !!! \n", nb_secret);
+        printf("\033[0m");    
+    } 
+    else {
 		printf("Echec");
 	}
 	return 0;
+
 }
